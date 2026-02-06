eb5d34bb2ade9fb51f313cba73272df05f54f389
8BitSoftware <159472061+8Bitsian@users.noreply.github.com>
2026-02-06 07:10:09 -0500

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+/*
+    Student Name: Iamni Hollie
+    File Name: script.js
+*/
+
+// jQuery for image to consume the header window space
+$(document).ready(function () {
+  $('.hero').height($(window).height());
+});
